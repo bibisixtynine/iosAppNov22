5830d83f03779df4b54aef3d39907bd81d7943e9
bibisixtynine <85257463+bibisixtynine@users.noreply.github.com>
2022-11-28 11:28:40 +0100

    Initial Commit

---
+++ iosAppNov22/iosAppNov22App.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppNov22App.swift
+//  iosAppNov22
+//
+//  Created by Jerome Binachon on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iosAppNov22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
